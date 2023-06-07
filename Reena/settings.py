36850eb27ea9cfe64459652f50dd4b06ae9afef5
Digamber Jha <kumardigamberjha@gmail.com>
2023-06-07 15:50:27 +0530

    updated images

--- Reena/settings.py
+++ Reena/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
    'default': {  
        'ENGINE': 'django.db.backends.mysql',  
        'NAME': 'reena', 
-       'PASSWORD':'dj@jha1011', 
+       'PASSWORD':'', 
        'USER': 'root',  
        'HOST': '127.0.0.1',  
        'PORT': '3306',  
